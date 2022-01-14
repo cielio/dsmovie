306d6cae4c26b1fb4ba27f556ea48703f28061ef
cielio <cielion@hotmail.com>
2022-01-14 11:17:48 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 //import './styles.css'
 function Listing() {
+
+  axios.get("http://localhost:8080/movies")
+    .then(response => {
+      console.log(response.data);
+    });
+
+
   return (<>
     <Pagination />
-
     <div className="container">
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
